78e752ffc952e19cd541f5eec64a49351b13f820
vicentefiorito <vicentefiorito@gmail.com>
2024-05-08 17:49:16 -0400

    testing DB connection

--- main.go
+++ main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/vicentefiorito/rss_feed_aggregator/internal/database"
 )
 
+// holds the stateful data
+type apiConfig struct {
+	DB *database.Queries
+}
+
 func main() {
+	// load the database
+	dbUrl := os.Getenv("DB_CONNECTION")
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("Problem connecting to the db!")
+	}
 	godotenv.Load(".env")
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("Port variable is not set!")
+	}
+
+	// stores the queries in a database package
+	dbQueries := database.New(db)
+
+	// config struct
+	apiConfig := apiConfig{
+		DB: dbQueries,
 	}
 
 	// creating a server
